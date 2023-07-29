eeb52cb66d81c812c47025429d81b67309b42fa8
leanzack <paninsorolean@gmail.com>
2023-07-29 15:29:55 +0800

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,8 @@
+
+        document.querySelector('form').addEventListener('submit', function () {
+            let name = document.querySelector('#name').value;
+            alert('hello,' + name);
+            event.preventDefault();
+        });
+
+    
