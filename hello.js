d848fe7f535154337ee97c2d695016eeeb168399
leanzack <138343724+leanzack@users.noreply.github.com>
2023-07-29 20:52:52 +0800

    2nd attemp

--- hello.js
+++ hello.js
@@ -5,13 +5,3 @@
             event.preventDefault();
         });
 
-    let body = document.querySelector('body');
-    document.querySelector('#red').addEventListener('Click', function() {
-        body.style.backgroundColor = 'red';
-        });
-    document.querySelector('#green').addEventListener('Click', function () {
-            body.style.backgroundColor = 'green';
-        });
-    document.querySelector('#blue').addEventListener('Click', function () {
-            body.style.backgroundColor = 'blue';
-        });
